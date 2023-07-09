c2d02685096fa5a3f03be9e6ddbc14ba87a2b8b6
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-07-10 06:18:24 +0800

    Update start_dml.bat

--- start_dml.bat
+++ start_dml.bat
@@ -7,7 +7,7 @@ python -m pip install virtualenv
 call install_dml.bat
 
 call venv\Scripts\activate.bat
-python run.py --execution-threads 1 --execution-provider dml --max-memory 16 --video-encoder libx264 --frame-processor face_swapper
+python run.py --execution-threads 1 --execution-provider dml --max-memory 16
 pause
 
 REM Упаковано и собрано телеграм каналом Neutogen News: https://t.me/neurogen_news
